59cfda6b572691b8b344240695e9bc8854b9bfc5
monirulislamriaj <riaj1132@gmail.com>
2021-01-03 17:11:42 +0600

    upded responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   "use strict";
-
+  //  Headroom Initialize
 });
 
 $(".widget-slider").slick({
@@ -28,73 +28,7 @@ $(".widget-slider").slick({
     },
   ],
 });
-// G-Map
-/**
- * Created by Kausar on 06/10/2016.
- */
-window.marker = null;
 
-function initialize() {
-  var map;
-  var nottingham = new google.maps.LatLng(23.7783741, 90.3746808);
-  var style = [
-    {
-      featureType: "administrative.country",
-      elementType: "geometry",
-      stylers: [
-        {
-          visibility: "simplified",
-        },
-        {
-          hue: "#ff0000",
-        },
-      ],
-    },
-  ];
-  var mapOptions = {
-    // SET THE CENTER
-    center: nottingham,
-    // SET THE MAP STYLE & ZOOM LEVEL
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    // SET THE BACKGROUND COLOUR
-    backgroundColor: "#000",
-    // REMOVE ALL THE CONTROLS EXCEPT ZOOM
-    zoom: 13,
-    panControl: false,
-    scrollwheel: false,
-    zoomControl: true,
-    mapTypeControl: false,
-    scaleControl: false,
-    streetViewControl: false,
-    overviewMapControl: false,
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.LARGE,
-    },
-  };
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  // SET THE MAP TYPE
-  var mapType = new google.maps.StyledMapType(style, {
-    name: "Grayscale",
-  });
-  map.mapTypes.set("grey", mapType);
-  map.setMapTypeId("grey");
-  //CREATE A CUSTOM PIN ICON
-  var marker_image = "images/pin.png";
-  var pinIcon = new google.maps.MarkerImage(
-    marker_image,
-    null,
-    null,
-    null,
-    new google.maps.Size(21, 34)
-  );
-  marker = new google.maps.Marker({
-    position: nottingham,
-    map: map,
-    icon: pinIcon,
-    title: "bizcred",
-  });
-}
-google.maps.event.addDomListener(window, "load", initialize);
 // sticky nav
 $(window).on("scroll", function () {
   if ($(window).scrollTop()) {
